fa641e487450446d1099b49f47f49a6a42855716
Nguyen-Duc-Trong <trongnguyen160902@gmail.com>
2024-07-30 20:51:50 +0700

    hihi16

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from 'express'
-// import cors from "cors"
+import cors from "cors"
 require('dotenv').config()
 import initRouter from './src/routers'
 import connectDatabase from './src/config/connectDatabase.js'
 
 const cors = require('cors');
 const app = express();
-app.use(cors({
-    // origin: process.env.CLIENT_URL,
-    // methods: ['POST', 'GET', 'PUT', 'DELETE']
-    origin: 'https://homestay-healing-client.vercel.app/'
-}))
+// app.use(cors({
+//     // origin: process.env.CLIENT_URL,
+//     // methods: ['POST', 'GET', 'PUT', 'DELETE']
+//     origin: 'https://homestay-healing-client.vercel.app/'
+// }))
+app.use(cors());
 //Đọc API từ Client gửi lên
 app.use(express.json())
 app.use(express.urlencoded({extended: true})) //Giúp đọc data dạng form data từ Client gửi lên
